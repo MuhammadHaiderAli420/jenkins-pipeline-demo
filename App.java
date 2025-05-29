94438ba369c5c6f60aaebf820868aedcdca116c8
MuhammadHaiderAli420 <93575502+MuhammadHaiderAli420@users.noreply.github.com>
2025-05-29 15:38:49 +1000

    Update App.java

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Haider");
+        System.out.println("Hello, Jenkins");
     }
 }
